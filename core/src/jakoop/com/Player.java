b38ed3b51e95a9454eb29b80a4b69480de68e804
210929 Bujnowicz Mateusz <210929@edu.p.lodz.pl>
2018-10-02 16:11:32 +0100

    Changed Player and Coin, Player can shoot and change coins

--- core/src/jakoop/com/Player.java
+++ core/src/jakoop/com/Player.java
@@ -1,5 +1,8 @@
 package jakoop.com;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
@@ -13,6 +16,9 @@ public class Player {
 	int currentCoin;
 	int lifes; 
     Texture texture;
+    ArrayList<Coin> coins;
+    
+    Coin staticCoin;
     
     final float SPEED = 600.0f; 
 
@@ -23,13 +29,21 @@ public class Player {
 	 posX = 640 - width/2; 
 	 posY = 180 - height/2;
 	currentCoin = Resources.ONE_EURO;
+	staticCoin = new Coin(currentCoin, 605, 15);
 	lifes = 3;
 	texture = new Texture(Gdx.files.internal("txapela.png"));
+	coins = new ArrayList<Coin>(); 
  }
  
  public void draw(SpriteBatch spriteBatch) {
 	 spriteBatch.draw(texture, posX, posY, width, height);
+	 staticCoin.drawStatic(spriteBatch);
 	 movement();
+	 for (int i = 0; i < coins.size(); i++) {
+		Coin coin = coins.get(i);
+		if(coin.isOut()) coins.remove(i);
+		coin.draw(spriteBatch);
+	}
  }
  
  public void movement() {
@@ -44,5 +58,27 @@ public class Player {
 	 if(posY<75) posY=75;
 	 if(posX>1280-width) posX=1280-width;
 	 if(posY>360-height) posY=360-height;
+	 
+	 if(Gdx.input.isKeyJustPressed(Keys.SHIFT_LEFT) || Gdx.input.isKeyJustPressed(Keys.SHIFT_RIGHT)) changeCoin();
+	 if(Gdx.input.isKeyJustPressed(Keys.SPACE)) coins.add(new Coin(currentCoin, posX+(width/2),posY+height ));
+ }
+ 
+ void changeCoin() {
+	 if(currentCoin == Resources.ONE_EURO) {
+		 currentCoin = Resources.TWO_EURO;
+		 staticCoin = new Coin(currentCoin, 605, 15);
+		 return;
+	 }
+	 if(currentCoin == Resources.TWO_EURO) {
+		 currentCoin = Resources.FIVE_EURO;
+		 staticCoin = new Coin(currentCoin, 605, 15);
+		 return;
+	 }
+	 if(currentCoin == Resources.FIVE_EURO) {
+		 currentCoin = Resources.ONE_EURO;
+		 staticCoin = new Coin(currentCoin, 605, 15);
+		 return;
+	 }
+	 
  }
 }
