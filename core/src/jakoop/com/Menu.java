35ffb2cada2f7867bb23b46833fe4a1d06c7a03e
aBarreiro <aitor.barreirom@lumni.mondragon.edu>
2018-10-10 16:22:43 +0200

    Final version v2.0

--- core/src/jakoop/com/Menu.java
+++ core/src/jakoop/com/Menu.java
@@ -9,7 +9,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.io.Writer;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
